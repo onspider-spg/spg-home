a21608a928be46b100824321f520cc90f1142b3e
onspider-spg <onspider@gmail.com>
2026-03-01 12:46:03 +1100

    Update api.js

--- js/api.js
+++ js/api.js
@@ -7,7 +7,7 @@
 
 const API = (() => {
   // ⚠️ CHANGE THIS after deploying Apps Script as Web App
-  let BASE_URL = localStorage.getItem('spg_api_url') || '';
+  let BASE_URL = localStorage.getItem('spg_api_url') || 'https://script.google.com/macros/s/AKfycbwMT-1g6fok_s7P454Lk-4zxTNeVD2zGZvFQyhua7kUFOuWAo0VsWcIXRxYH3GpQgSk/exec';
 
   // Session storage
   const SESSION_KEY = 'spg_session';
